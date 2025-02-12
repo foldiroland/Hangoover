51acf5e89a5d2de3bc3e17320e9448a0c21f5d74
Kedves <foldyroland@gmail.com>
2025-02-12 09:39:12 +0100

    big update

--- main.php
+++ main.php
@@ -10,10 +10,126 @@
     <div class="content">
         <div class="Hangoover">
                 <img src="" alt="">
-                <h1>bal oldal</h1>
+                <br><br><br><br>
+                <img src="card.png" alt="">
+                <div class="taskInfo">
+                    <h1>Current task: </h1>
+                    <h3 id="taskName">Brand Ring</h3>
+                    <p id="task">Everyone says a brand,who can’t, has to drink a sip</p>
+                </div>
+                <div class="navigate">
+                    <br><br><br><br><br>
+                    <button id="dice"><img src="dice.png" width="200px"></button>
+                </div>
+                
+                
         </div>
-        <div class="HangooverBack" id="">
-            <h1>Jobb oldal</h1>
+        <div class="HangooverBack" id="segs" >
+        
+        <!-- 41 cella van tehát 41 lépés érhető el  -->
+
+            <table class="cock" border="0px">
+                
+                        <!-- 1 sor -->
+                <tr>
+                    <th id="cell" id="1"></th>
+                    <th></th>
+                    <th id="cell" id="2"></th>
+                    <th id="cell" id="3"></th>
+                    <th id="cell" id="4"></th>
+                    <th></th>
+                    <th id="cell" id="5"></th>
+                    <th></th>
+                    <th id="cell" id="6"></th>
+                    <th></th>
+                    <th id="cell" id="7"></th>
+                </tr>
+                        <!-- 2 sor -->
+                <tr>
+                    <th id="cell" id="8"></th>
+                    <th></th>
+                    <th id="cell" id="9"></th>
+                    <th></th>
+                    <th id="cell" id="10"></th>
+                    <th></th>
+                    <th id="cell" id="11"></th>
+                    <th></th>
+                    <th id="cell" id="12"></th>
+                    <th></th>
+                    <th id="cell" id="13"></th>
+                </tr>
+                        <!-- 3 sor -->
+                <tr>
+                    <th id="cell" id="14"></th>
+                    <th></th>
+                    <th id="cell" id="15"></th>
+                    <th></th>
+                    <th id="cell" id="16"></th>
+                    <th></th>
+                    <th id="cell" id="17"></th>
+                    <th></th>
+                    <th id="cell" id="18"></th>
+                    <th></th>
+                    <th id="cell" id="19"></th>
+                </tr>
+                        <!-- 4 sor -->
+                <tr>
+                    <th id="cell" id="20"></th>
+                    <th></th>
+                    <th id="cell" id="21"></th>
+                    <th></th>
+                    <th id="cell"  id="22"></th></th>
+                    <th></th>
+                    <th id="cell" id="23"></th>
+                    <th></th>
+                    <th id="cell" id="24"></th>
+                    <th></th>
+                    <th id="cell" id="25"></th>
+                </tr>
+                        <!-- 5 sor -->
+                <tr>
+                    <th id="cell" id="26">sdedfcfljkdfsglkfdb</th>
+                    <th></th>
+                    <th id="cell" id="27">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="28">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="29">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="30">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="31">awrdwadfawdwa</th>
+                </tr>
+                        <!-- 6 sor -->
+                <tr>
+                    <th id="cell" id="32">sdedfcfljkdfsglkfdb</th>
+                    <th></th>
+                    <th id="cell" id="33">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="34">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="35">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="36">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="37">awrdwadfawdwa</th>
+                </tr>
+                        <!-- 7 sor -->
+                <tr>
+                    <th id="cell" id="38">sdedfcfljkdfsglkfdb</th>
+                    <th id="cell" id="39">dxaeswfwerew</th>
+                    <th id="cell" id="40">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell" id="41">awrdwadfawdwa</th>
+                    <th id="cell" id="42">awrdwadfawdwa</th>
+                    <th id="cell"id="43">awrdwadfawdwa</th>
+                    <th></th>
+                    <th id="cell"id="44">awrdwadfawdwa</th>
+                    <th id="cell"id="45">awrdwadfawdwa</th>
+                    <th id="cell"id="46">awrdwadfawdwa</th>
+                </tr>                
+            </table>
+            
         </div>
     </div>
     
